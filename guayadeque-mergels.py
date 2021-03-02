4a3da328db5710b28a2a9945e285723d7a202213
openmonk <akuznetsou@libertyglobal.com>
2021-03-02 13:12:27 +0100

    bugfix

--- guayadeque-mergels.py
+++ guayadeque-mergels.py
@@ -5,11 +5,12 @@ import os, sys
 
 n_lss = ET.fromstringlist(len(sys.argv)>1 and open(sys.argv[1], 'r').readlines() or sys.stdin.readlines()).findall('lyricsources/lyricsource')
 o_xml = ET.parse(os.getenv('GDQ_LYRICS_SOURCES', os.getenv('HOME','') + '/.guayadeque/lyrics_sources.xml'))
+o_dest = o_xml.find('lyricsources')
 
 for n_ls in n_lss:
 	o_lss = o_xml.findall('lyricsources/lyricsource[@name="%s"]' % (n_ls.attrib['name'], ))
 	if not o_lss:
-		o_xml.append(n_ls)
+		o_dest.append(n_ls)
 	else:
 		for o_ls in o_lss:
 			enabled = o_ls.attrib['enabled']
